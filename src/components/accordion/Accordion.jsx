c43530cd817783e8e846249206a73be2e56eb257
Parham Danesh <parhamdaneshnejad@gmail.com>
2026-07-18 21:04:06 +0330

    Added FAQ datas and completed FAQ page

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,18 +1,18 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { SlArrowDown } from "react-icons/sl";
-export default function Accordion() {
+export default function Accordion({ id, question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="bg-[#fbf6f0] dark:bg-gray-600 dark:text-white border rounded-xl">
+    <div className="bg-[#ece3d8] dark:bg-gray-600 dark:text-white border rounded-xl p-3 font-estedad transition-colors duration-500">
       <button
-        className="flex w-full justify-between p-3"
-        id="faq-question-1"
+        className="flex w-full justify-between cursor-pointer px-3"
+        id={`faq-question-${id}`}
         aria-expanded={isOpen}
-        aria-controls="faq-answer-1"
+        aria-controls={`faq-answer-${id}`}
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <span>سلام چطوری؟ خوبی؟ چه خبره؟</span>
+        <span>{question}</span>
         <SlArrowDown
           className={`transition-transform duration-300 ${isOpen ? "rotate-180" : ""}`}
           aria-hidden="true"
@@ -20,18 +20,14 @@ export default function Accordion() {
       </button>
       <motion.div
         initial={{ height: 0 }}
-        animate={isOpen ? { height: "auto" } : { height: 0 }}
+        animate={isOpen ? { height: "auto", paddingTop: 10 } : { height: 0 }}
         transition={{ duration: 0.3 }}
-        className="overflow-hidden"
-        id="faq-answer-1"
+        className={`overflow-hidden px-3`}
+        id={`faq-answer-${id}`}
         role="region"
-        aria-labelledby="faq-question-1"
+        aria-labelledby={`faq-question-${id}`}
       >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          asperiores, earum, omnis possimus repudiandae inventore, voluptate non
-          ullam voluptatibus quod dignissimos. Cum, perferendis!
-        </p>
+        <p>{answer}</p>
       </motion.div>
     </div>
   );
